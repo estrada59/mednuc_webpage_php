71624c0e1eb640358eb810f53eac3242be32bbf0
enrique estrada <estrada59@gmail.com>
2021-01-15 13:15:26 -0600

    Se habilito un include para que los datos de experiencia tengan un sólo punto de entrada

--- gammagrama_pulmonar.php
+++ gammagrama_pulmonar.php
@@ -58,31 +58,9 @@
                 
             </div><!-- container -->
 
-            <section class="full-section dark-section parallax" id="section-2" data-stellar-background-ratio="0.3">
-
-                <div class="full-section-overlay-color"></div>
-    
-                <div class="full-section-container">
-    
-                    <div class="container">
-                        <div class="row align-items-center">
-                            <div class="col-lg-8">
-    
-                                <h1>17 Años de experiencia en Tuxtla Gutiérrez</h1>
-                                <p class="last">Hemos hecho una gran variedad de estudios para el dianóstico y tratamiento 
-                                    de varios padecimientos.</p>
-    
-                            </div><!-- col -->
-                            <div class="col-lg-4 text-lg-right">
-    
-                                <a class="btn btn-default waves mb-2 mb-md-0 mt-3 mt-lg-0" href="contacto.php">Comuníquese con nosotros</a>
-    
-                            </div><!-- col -->
-                        </div><!-- row -->
-                    </div><!-- container -->
-    
-                </div><!-- full-section-container -->
-            </section><!-- full-section -->
+            <?php 
+                include_once 'seccion_experiencia.php';
+            ?>
 
             <div class="container">
                 <div class="row">
