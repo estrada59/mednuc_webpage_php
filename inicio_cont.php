d224e05c1f57c25bdeba6eb3408d5e53ba0a6f64
enrique estrada <estrada59@gmail.com>
2021-01-15 13:15:58 -0600

    se habilito include total de pacientes el cual consulta la base de datos

--- inicio_cont.php
+++ inicio_cont.php
@@ -97,54 +97,9 @@
             </div><!-- row -->
         </div><!-- container -->
 
-      
-        <section class="full-section dark-section parallax" id="section-1" data-stellar-background-ratio="0.3">
-
-            <div class="full-section-overlay-color"></div>
-
-            <div class="full-section-container">
-
-                <div class="container">
-                    <div class="row">
-
-                        <div class="col-md-4 col-sm-4">
-
-                            <div class="counter">
-
-                                <i class="smartmed-icon-prisoner"></i>
-                                <div class="counter-value" data-value="4270"></div>
-                                <div class="counter-details">Pacientes atendidos</div>
-
-                            </div><!-- counter -->
-
-                        </div><!-- col -->
-                        <div class="col-md-4 col-sm-4">
-
-                            <div class="counter">
-
-                                <i class="smartmed-icon-judge"></i>
-                                <div class="counter-value" data-value="1"></div>
-                                <div class="counter-details">Doctor</div>
-
-                            </div><!-- counter -->
-
-                        </div><!-- col -->
-                        <div class="col-md-4 col-sm-4">
-
-                            <div class="counter">
-
-                                <i class="smartmed-icon-typewriter"></i>
-                                <div class="counter-value" data-value="80" data-symbol-before="+" data-symbol-after=""></div>
-                                <div class="counter-details">Servicios</div>
-
-                            </div><!-- counter -->
-
-                        </div><!-- col -->
-                    </div><!-- row -->
-                </div><!-- container -->
-
-            </div><!-- full-section-container -->
-        </section><!-- full-section -->
+        <?php 
+            include_once 'seccion_total_pacientes.php';
+        ?>
 
         <div class="container">
             <div class="row">
@@ -221,31 +176,9 @@
             </div><!-- row -->
         </div><!-- container -->
 
-        <section class="full-section dark-section parallax" id="section-2" data-stellar-background-ratio="0.3">
-
-            <div class="full-section-overlay-color"></div>
-
-            <div class="full-section-container">
-
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-lg-8">
-
-                            <h1>17 Años de experiencia en Tuxtla Gutiérrez</h1>
-                            <p class="last">Hemos hecho una gran variedad de estudios para el dianóstico y tratamiento 
-                                de varios padecimientos.</p>
-
-                        </div><!-- col -->
-                        <div class="col-lg-4 text-lg-right">
-
-                            <a class="btn btn-default waves mb-2 mb-md-0 mt-3 mt-lg-0" href="contacto.php">Comuníquese con nosotros</a>
-
-                        </div><!-- col -->
-                    </div><!-- row -->
-                </div><!-- container -->
-
-            </div><!-- full-section-container -->
-        </section><!-- full-section -->
+        <?php 
+            include_once 'seccion_experiencia.php';
+        ?>
 
         <div class="container">
             <div class="row">
--- nosotros_cont.php
+++ nosotros_cont.php
@@ -59,53 +59,9 @@
                 </div><!-- row -->
             </div><!-- container -->
 
-            <section class="full-section dark-section parallax" id="section-1" data-stellar-background-ratio="0.3">
-
-                <div class="full-section-overlay-color"></div>
-
-                <div class="full-section-container">
-
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-4 col-sm-4">
-
-                                <div class="counter">
-
-                                    <i class="smartmed-icon-gavel"></i>
-                                    <div class="counter-value" data-value="4270"></div>
-                                    <div class="counter-details">Pacientes atendidos</div>
-
-                                </div><!-- counter -->
-
-                            </div><!-- col -->
-                            
-                            <div class="col-md-4 col-sm-4">
-
-                                <div class="counter">
-
-                                    <i class="smartmed-icon-judge"></i>
-                                    <div class="counter-value" data-value="1"></div>
-                                    <div class="counter-details">Doctor</div>
-
-                                </div><!-- counter -->
-
-                            </div><!-- col -->
-                            <div class="col-md-4 col-sm-4">
-
-                                <div class="counter">
-
-                                    <i class="smartmed-icon-typewriter"></i>
-                                    <div class="counter-value" data-value="80" data-symbol-before="+" data-symbol-after=""></div>
-                                    <div class="counter-details">Servicios</div>
-
-                                </div><!-- counter -->
-
-                            </div><!-- col -->
-                        </div><!-- row -->
-                    </div><!-- container -->
-
-                </div><!-- full-section-container -->
-            </section><!-- full-section -->
+            <?php 
+                include_once 'seccion_total_pacientes.php';
+            ?>
 
             <div class="container">
                 <div class="row">
