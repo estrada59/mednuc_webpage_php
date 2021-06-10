a05da6a1283ac277b31bfad5436f8ddf363853a4
Enrique Estrada <estrada59@gmail.com>
2021-06-09 22:44:06 -0500

    Se agrego analitics a numedics

--- script.php
+++ script.php
@@ -63,14 +63,27 @@
 <!-- CUSTOM JS -->
 <script src="assets/js/custom.js"></script>
 
-<!-- Google Analytics -->
+<!-- Google Analytics medicinanucleardechiapas.com-->
 <script>
-        // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        
-        // ga('create', 'UA-27765610-1', 'auto');
-        // ga('send', 'pageview');
-    </script> 
+    // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+    // ga('create', 'UA-27765610-1', 'auto');
+    // ga('send', 'pageview');
+</script> 
+
+<!-- Google Analytics numedics.com.mx-->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-27765610-2"></script>
+<script>
+    // window.dataLayer = window.dataLayer || [];
+    // function gtag(){dataLayer.push(arguments);}
+    // gtag('js', new Date());
+
+    // gtag('config', 'UA-27765610-2');
+</script>
+
+
+    
 
